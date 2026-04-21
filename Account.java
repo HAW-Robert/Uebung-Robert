1b222be620cc5306c1e65233e1e7743bf28fed1b
HAW-Robert <robertarnds1207gmail.com>
2026-04-21 11:51:24 +0200

    Account Prohgramm in dieses Repository Übertragen.

---
+++ Account.java
@@ -0,0 +1,28 @@
+public class Account {
+    private int accountnumber;
+    private double balance;
+
+//getters setters
+public void setAccountnumber(int newNumber){
+      accountnumber = newNumber;
+}
+public int getAccountnumber(){
+    return  accountnumber;
+}
+public void setBalance(double newBalance){
+    balance = newBalance;
+}
+public double getBalance(){
+    return balance;
+}
+public void info(){
+    System.out.println("Accountnumber :" + accountnumber);
+    System.out.println("Balance :" + balance);
+}
+public void main(String[] args){
+    Account myAccount = new Account();
+    myAccount.setAccountnumber(420);
+    myAccount.setBalance(300);
+    myAccount.info();
+}
+}
