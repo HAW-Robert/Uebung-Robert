599216ab78434940eb9d2e72f82598d0c033f578
HAW-Robert <robertarnds1207gmail.com>
2026-05-13 11:39:05 +0200

    transaction unterklasse plus funktionen hinzugefügt

--- Account/Account.java
+++ Account/Account.java
@@ -2,7 +2,24 @@ package Account;
 public class Account {
     private int accountnumber;
     private double balance;
+    private Transaction last;
 
+public class Transaction{
+    private String name;
+    private double amount;
+
+public Transaction(String name, double amount){
+    this.name = name;
+    this.amount = amount;
+}
+//Funktion kann ein Objekt bzw den Inhalt als String wiedergeben 
+public String toString(){
+    return "Transaction : "+ name +" : "+ amount;
+}
+public Transaction getLast(){
+    return last;
+}
+}
 //getters setters
 public void setAccountnumber(int newNumber){
       accountnumber = newNumber;
@@ -19,16 +36,18 @@ public double getBalance(){
 public void info(){
     System.out.println("Accountnumber :" + accountnumber);
     System.out.println("Balance :" + balance);
+    System.out.println("Letzte Kontobewegung :"+ last);
 }
 public void payIn(double amount){
     balance += amount;
-
+    last =new Transaction(" Kindergeld", amount);
 }
 
 public void main(String[] args){
     Account myAccount = new Account();
     myAccount.setAccountnumber(420);
     myAccount.setBalance(300);
+    myAccount.payIn(420);
     myAccount.info();
 }
 }
